7b00aebd3146b206b002f5998cb4e5098251b360
nuliswathi <nuliswathi@github.com>
2020-10-23 13:41:27 +0530

    push files

---
+++ build/precache-manifest.b04231496092561a9cf4cde88b6197b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40af9358633bac0b3ebff15182f12cc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eca6d1c696a43445487a",
+    "url": "/static/css/main.1e9be16b.chunk.css"
+  },
+  {
+    "revision": "a27cb37874585d64995f",
+    "url": "/static/js/2.2d3ef7d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2d3ef7d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eca6d1c696a43445487a",
+    "url": "/static/js/main.98e3860b.chunk.js"
+  },
+  {
+    "revision": "2f9336c112ea9e912473",
+    "url": "/static/js/runtime-main.7e6df230.js"
+  }
+]);
