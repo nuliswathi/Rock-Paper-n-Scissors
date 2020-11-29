0128ef0d85e06973e77ef2ac5f80b8dd15bf73f9
nuliswathi <nuliswathi@github.com>
2020-11-29 13:38:13 +0530

    added return false

--- src/GamePage.js
+++ src/GamePage.js
@@ -9,9 +9,11 @@ class GamePage extends Component {
         var comp_pattern = this.props.pattern;
         if(user_pattern === comp_pattern) {
             alert("You have guessed the right pattern hurray!!");
+            return false;
         }
         else {
             alert("The pattern you entered is wrong!!");
+            return false;
         }
     }
     checkWinner = (event) => {
